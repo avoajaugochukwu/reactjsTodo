71ccb9a5533b30dfe57994fa6b06d37f51ee6981
avoajaugochukwu <karlthegoo@hotmail.com>
2017-01-05 23:11:35 +0100

    new form of creating components

---
+++ js/learn.js
@@ -0,0 +1,10 @@
+class Layout extends React.Component {
+  render() {
+    return (
+      <h1>It works!</h1>
+    );
+  }
+}
+
+const app = document.getElementById('app');
+ReactDOM.render(<Layout/>, app);
